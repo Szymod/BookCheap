4b53c8c748d736f732e4c0af9cf617d529d91318
kmakowski21 <kamil.mak@wp.pl>
2016-01-06 20:39:08 +0100

    Pobieranie listy hoteli

---
+++ Clients/WebClient/Controllers/HotelListController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookCheap.Busines.DomainModel;
+using BookCheap.Clients.WebClient.App_Start;
+using BookCheap.Persistence.DataAccess;
+
+namespace BookCheap.Clients.WebClient.Controllers
+{
+    public class HotelListController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: HotelList
+        public ActionResult Index()
+        {
+            return View(db.Hotels.ToList());
+        }
+    }
+}
