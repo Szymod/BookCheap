c4360cb0a048d1ad0f2a9d5dcd8291071c786608
jjlenart <lenartjedrzej@gmail.com>
2016-01-16 19:29:03 +0100

    test

--- Clients/DesktopClient/Logowanie.cs
+++ Clients/DesktopClient/Logowanie.cs
@@ -21,6 +21,7 @@ namespace BookCheap.Clients.DesktopClient
         private void button1_Click(object sender, EventArgs e)
         {
             //odwołanie do WebServicu zrobionego przez Jędrzeja, jak w końcu go zrobi :)
+            //czyli już niebawem ;)
         }
     }
 }
