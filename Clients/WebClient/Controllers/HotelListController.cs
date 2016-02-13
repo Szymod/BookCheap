58d3a6ef541c6d253bbab4dddff231c8597590bc
kmakowski21 <kamil.mak@wp.pl>
2016-02-13 04:19:16 +0100

    Sortowanie listy hoteli

--- Clients/WebClient/Controllers/HotelListController.cs
+++ Clients/WebClient/Controllers/HotelListController.cs
@@ -15,11 +15,36 @@ namespace BookCheap.Clients.WebClient.Controllers
         //private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: HotelList
-        public ActionResult Index(int? page)
+        public ActionResult Index(int? page, string sortOrder)
         {
             int currentPage = page ?? 1;
             int pageSize = 5;
+
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSort = String.IsNullOrEmpty(sortOrder) ? "" : "Name";
+            ViewBag.RatingSort = String.IsNullOrEmpty(sortOrder) ? "RatingAsc" : "Rating";
+
             var hotels = UnitOfWork.Hotels.GetAll().OrderBy(n => n.Name);
+
+            switch (sortOrder)
+            {
+                //case "Rating":
+                //    hotels.OrderByDescending(s => s.Rating);
+                //    break;
+
+                //case "RatingAsc":
+                //    hotels.OrderBy(s => s.Rating);
+                //    break;
+
+                case "Name": 
+                    hotels.OrderByDescending(s => s.Name);
+                    break;
+
+                default:
+                    hotels.OrderBy(s => s.Name);
+                    break;
+            }
+
             return View(hotels.ToPagedList<Hotel>(currentPage,pageSize));
         }
 
